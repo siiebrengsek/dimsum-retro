c50ea7645204fc1e7f091215a933dd2dcc01f8bc
hachi <siiebrengsek@gmail.com>
2026-05-15 01:57:10 +0700

    fix(products): offset sticky top by navbar height (88px)

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -111,7 +111,7 @@ const Products = () => {
       </div>
 
       {/* Category Filter */}
-      <div className="sticky top-0 z-10 bg-gray-50 border-b border-gray-200">
+      <div className="sticky top-[88px] z-10 bg-gray-50 border-b border-gray-200">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
           <div className="flex flex-wrap justify-center gap-4">
             {categories.map((category) => (
